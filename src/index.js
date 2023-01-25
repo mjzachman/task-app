2701c6bc7d14460fb743bd1f0f6d83e2aa97041b
Matt Zachman <mzachman14@gmail.com>
2023-01-25 17:49:16 -0500

    Edits

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+// index.js
 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,5 +10,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-
