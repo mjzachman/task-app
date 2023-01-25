583310d1a25affa08936e42c19a83af59d6e3f0f
Matt Zachman <mzachman14@gmail.com>
2023-01-25 17:42:53 -0500

    Create skeleton of App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+// App.js
 
-function App() {
-  return (
-    <div></div>
-  );
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor() {
+    super();
+
+
+    this.state ={
+      task: {text: ''},
+      tasks: [],
+    };
+  }
+
+
+  render() {
+    const {task, tasks } = this.state;
+
+
+    return (
+    <div>
+      <form>
+        <label htmlFor="taskInput">Enter task</label>
+        <input type="text" id="taskInput"/>
+        <button type="submit">
+          Add Task
+        </button>
+      </form>
+    </div>
+    );
+  }
 }
 
 export default App;
